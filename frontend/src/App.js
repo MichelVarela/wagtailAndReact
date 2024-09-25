9b43168a3309b0ed79bde6d6d59004345d3e9b58
michelvarela <michelvarela.95@outlook.com>
2024-09-25 19:36:00 -0300

    add animation, map id and paragraph ok

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,22 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import DOMPurify from 'dompurify';
-import { Container, Heading, Box, Image } from '@chakra-ui/react'
+import { Container, Flex, Heading, Box, Image, keyframes } from '@chakra-ui/react';
+import { SpinnerIcon } from '@chakra-ui/icons';
+import { motion } from 'framer-motion';
 
 
 axios.defaults.baseURL = process.env.REACT_APP_API_URL
+
+const animationKeyframes = keyframes`
+  0% { transform: scale(1) rotate(0); border-radius: 20%; }
+  25% { transform: scale(2) rotate(0); border-radius: 20%; }
+  50% { transform: scale(2) rotate(270deg); border-radius: 50%; }
+  75% { transform: scale(1) rotate(270deg); border-radius: 50%; }
+  100% { transform: scale(1) rotate(0); border-radius: 20%; }
+`;
+
+const animation = `${animationKeyframes} 2s ease-in-out infinite`;
 
 function App() {
     const [loading, setLoading] = useState(false);
@@ -19,29 +31,25 @@ function App() {
         
         try {
             setLoading(true)
-            const result = await axios.get(`${process.env.REACT_APP_API_URL}/pages/?type=home.HomePage&fields=header,body&id=` + id)
+            const result = await axios.get(`${process.env.REACT_APP_API_URL}/api/v2/pages/?type=home.HomePage&fields=header,body&id=` + id);
             
             setData(result.data.items);
             setBody(result.data.items[0].body);
             setLoading(false);
             
-            let html = [];
+            let paragraphs = [];
             for (let i = 0; i < body.length; i++) {          
                 
                 if (body[i].type === 'image') { 
-                    const results = await axios.get(`${process.env.REACT_APP_API_URL}/images/` + body[i].value)
+                    const results = await axios.get(`${process.env.REACT_APP_API_URL}/api/v2/images/` + body[i].value)
                     .then(results => {                    
                         setImage(results.data.meta.download_url)
                     })
                 }
                 
                 else if (body[i].type === 'paragraph') {
-                    html.push(
-                        <Box>
-                            <Box dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(body[i].value) }} />
-                        </Box>
-                    );
-                    setParagraph(html)
+                    paragraphs.push(body[i]);
+                    setParagraph(paragraphs);
                 }
             }          
         }
@@ -60,9 +68,21 @@ function App() {
     
     if (loading) {
         return (
-            <h4>
-            <img src="https://i.pinimg.com/originals/48/6a/a0/486aa0fa1658b7522ecd8918908ece40.gif" />
-            </h4>
+            <Container h="100vh" display="flex" alignItems="center" justifyContent="center">
+                <Box as={motion.div}
+                    animation={animation}
+                    // not work: transition={{ ... }}
+                    padding="2"
+                    // @ts-ignore - "Does not exist" Type Error against Motion
+                    bgGradient="linear(to-l, #7928CA, #FF0080)"
+                    width="12"
+                    height="12"
+                    display="flex"
+                    justifyContent="center"
+                    alignItems="center">
+                    <SpinnerIcon color="white"/>
+                </Box>
+            </Container>
         );
     } 
     
@@ -72,20 +92,17 @@ function App() {
                 <Image src={`${process.env.REACT_APP_API_URL}/${image}`} alt='Dan Abramov' />
             </Box>
         
-            <div>
-            {data.map((data) => (
-                <ul key={data.id}>
-                {data.title} <br />
-                {data.description}
-                </ul>))}            
-            </div>
+            <Box>
+                {data.map((data) => (
+                <Box key={data.id}>
+                    <Heading>{data.title}</Heading>
+                </Box>))}            
+            </Box>
 
-            <div>
-            {paragraph.map((paragraph) => (
-            <ul>
-                {paragraph}
-            </ul>))}
-            </div>
+            <Flex direction="column" gap="1.5rem">
+                {paragraph.map((paragraph) => (
+                <Box key={paragraph.id} dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(paragraph.value) }}/>))}
+            </Flex>
         </Container>
     );
 }
